d9c8511fdc6e1426073ad2a12caaa3f9fb9dac01
Екатерина Гордеева <katiyhagordeeva@mail.ru>
2023-11-16 23:29:30 +0300

    Добавьте файлы проекта.

---
+++ IsMatrixSymmetric/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер матрицы (количество строк и столбцов): ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        int[,] array = new int[a, a];
+        Random random = new Random();
+
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < a; j++)
+            {
+                array[i, j] = random.Next(1, 11);
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        bool isSymmetric = CheckSymmetry(array);
+
+        if (isSymmetric)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+
+    private static bool CheckSymmetry(int[,] array)
+    {
+        int length = array.GetLength(0);
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                if (array[i, j] != array[j, i])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
